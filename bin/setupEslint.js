4b15e211932728b3df4b0814942ebf37fa604d76
Adebayo Opesanya <onejsninja@Adebayos-MacBook-Pro-2.local>
2018-11-09 13:02:29 +0100

    create function to setup eslint

--- bin/setupEslint.js
+++ bin/setupEslint.js
@@ -3,15 +3,13 @@
 const shell = require('shelljs');
 const fs = require('fs');
 
-const setupEslint = () => {
-  shell.exec('npm i -D eslint eslint-config-airbnb-base@latest eslint-plugin-import');
+const setupEslint = (folderName) => {
+  shell.exec(`cd ${folderName} && npm i -D eslint eslint-config-airbnb-base@latest eslint-plugin-import`);
 
   const eslintConfig = {
     extends: 'airbnb-base',
   };
-
-
-  fs.writeFile('../eslintrc.json', JSON.stringify(eslintConfig, null, 2), (err) => {
+  fs.writeFile(`${folderName}/eslintrc.json`, JSON.stringify(eslintConfig, null, 2), (err) => {
     if (err) return console.log(err);
   });
 };
